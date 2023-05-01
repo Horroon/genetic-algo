6157dfcfbf0bc42d2c84ff2b421d17d18e45e1fb
haroon ul rasheed <haroon.rasheed@intechww.com>
2023-05-01 12:08:31 +0500

    algo updated

--- controllers/cuttingPlan.js
+++ controllers/cuttingPlan.js
@@ -1,9 +1,9 @@
-const { generatePlan } = require("../models/cuttingPlan");
+const { fabricCuttingPlan } = require("../models/cuttingPlan");
 
 const CuttingPlanController = (req, res) => {
   try {
-    const { cuttingPlan } = generatePlan({...req.body});
-    res.status(201).json({ cuttingPlan });
+    const resp = fabricCuttingPlan({ ...req.body });
+    res.status(201).json(resp);
   } catch (error) {
     res.status(401).json({ error });
   }
--- models/cuttingPlan.js
+++ models/cuttingPlan.js
@@ -1,96 +1,105 @@
-function generatePlan({
-    maxGarments,
-    minGarments,
-    maxMarkers,
-    minMarkers,
-    cutOrder = [
-      { size: 38, style: "A", quantity: 54 },
-      { size: 40, style: "A", quantity: 84 },
-      { size: 42, style: "A", quantity: 91 },
-      { size: 44, style: "B", quantity: 60 },
-      { size: 46, style: "B", quantity: 29 },
-    ],
-  }) {
-    // Define the fabric width and length in inches
-    // const fabricWidth = 60;
-    // const fabricLength = 120;
-  
-    // Define the maximum and minimum number of garments and sizes that can be allocated for a marker
-    const gmx = maxGarments;
-    const gmin = minGarments;
-    const dmax = maxMarkers;
-    // const dmin = minMarkers;
-  
-    // Initialize the cut plan as an empty array of markers
-    const cutPlan = [];
-  
-    // Initialize the remaining demand for each size and style to the quantities in the cut order
-    const demands = {};
-    for (const { size, style, quantity } of cutOrder) {
-      demands[`${size}${style}`] = quantity;
-    }
-  
-    for (const { size } of cutOrder) {
-      let sizeDemand = cutOrder.reduce(
-        (acc, { size: s, quantity }) => (s === size ? acc + quantity : acc),
-        0
-      );
-  
-      for (const { style } of cutOrder) {
-        const sizeDemandKey = `${size}${style}`;
-        let sizePieces = demands[sizeDemandKey];
-  
-        while (sizePieces > 0) {
-          const marker = { size, style, pieces: [] };
-  
-          let garments = Math.min(gmx, sizePieces);
-  
-          if (sizePieces <= gmx * dmax && garments < gmin) {
-            garments = gmin;
-          }
-  
-          for (const { size: s, style: d } of cutOrder) {
-            if (s === size && d === style) {
-              continue;
-            }
-  
-            const remainingDemandKey = `${s}${d}`;
-            const remainingDemand = demands[remainingDemandKey];
-            if (remainingDemand === 0) {
-              continue;
-            }
-  
-            let maxGarments = Math.min(gmx, garments, remainingDemand);
-  
-            let maxSizes = Math.min(
-              dmax,
-              Math.ceil((maxGarments * dmax) / garments),
-              Math.ceil(remainingDemand / dmax)
-            );
-  
-            if (sizePieces <= gmx * dmax && maxGarments < gmin) {
-              maxGarments = gmin;
-              maxSizes = 1;
-            }
-  
-            const pieces = Math.min(maxGarments * maxSizes, remainingDemand);
-  
-            for (let i = 0; i < pieces; i++) {
-              marker.pieces.push({ size: s, style: d });
-            }
-  
-            demands[remainingDemandKey] -= pieces;
-  
-            sizePieces -= pieces;
-            garments -= Math.floor((pieces - 1) / maxSizes) + 1;
-          }
-  
-          cutPlan.push(marker);
-        }
+function fabricCuttingPlan({
+  fabricWidth,
+  fabricHeight,
+  pieces,
+  populationSize = 10,
+  maxIterations = 100,
+  mutationProbability = 0.1,
+  tournamentSize = 3,
+}) {
+    
+  // Create the initial population
+  let population = [];
+  for (let i = 0; i < populationSize; i++) {
+    let pattern = [];
+    let remainingFabric = { width: fabricWidth, height: fabricHeight };
+    let availablePieces = [...pieces]; // make a copy of the original pieces array
+    while (availablePieces.length > 0) {
+      let pieceIndex = Math.floor(Math.random() * availablePieces.length);
+      let piece = availablePieces[pieceIndex];
+      if (
+        piece.width <= remainingFabric.width &&
+        piece.height <= remainingFabric.height
+      ) {
+        pattern.push(piece);
+        remainingFabric.width -= piece.width;
+        remainingFabric.height -= piece.height;
       }
+      availablePieces.splice(pieceIndex, 1); // remove the selected piece from the available pieces
     }
-  
-    return { cuttingPlan: cutPlan };
+    population.push(pattern);
   }
-  
-  module.exports = { generatePlan };  
+
+  // Evaluate the fitness
+  function evaluateFitness(pattern) {
+    let totalArea = fabricWidth * fabricHeight;
+    let usedArea = pattern.reduce(
+      (acc, piece) => acc + piece.width * piece.height,
+      0
+    );
+    let wastedArea = totalArea - usedArea;
+    return wastedArea;
+  }
+  population = population.map((pattern) => ({
+    pattern,
+    fitness: evaluateFitness(pattern),
+  }));
+
+  // Genetic algorithm loop
+  for (let iteration = 1; iteration <= maxIterations; iteration++) {
+    // Select parents
+    function tournamentSelection(population, tournamentSize) {
+      let candidates = [];
+      for (let i = 0; i < tournamentSize; i++) {
+        let index = Math.floor(Math.random() * population.length);
+        candidates.push(population[index]);
+      }
+      candidates.sort((a, b) => a.fitness - b.fitness);
+      return candidates[0].pattern;
+    }
+    const parent1 = tournamentSelection(population, tournamentSize);
+    const parent2 = tournamentSelection(population, tournamentSize);
+
+    // Crossover
+    const crossoverPoint = Math.floor(Math.random() * parent1.length);
+    let child = [
+      ...parent1.slice(0, crossoverPoint),
+      ...parent2.slice(crossoverPoint),
+    ];
+
+    // Mutation
+    function randomMutation(pattern) {
+      let mutatedPattern = [...pattern];
+      let index1 = Math.floor(Math.random() * mutatedPattern.length);
+      let index2 = Math.floor(Math.random() * mutatedPattern.length);
+      [mutatedPattern[index1], mutatedPattern[index2]] = [
+        mutatedPattern[index2],
+        mutatedPattern[index1],
+      ];
+      return mutatedPattern;
+    }
+    if (Math.random() < mutationProbability) {
+      child = randomMutation(child);
+    }
+    // Evaluate fitness
+    const childFitness = evaluateFitness(child);
+
+    // Replace the least fit individual
+    population.sort((a, b) => b.fitness - a.fitness);
+    if (childFitness < population[populationSize - 1].fitness) {
+      population[populationSize - 1] = {
+        pattern: child,
+        fitness: childFitness,
+      };
+    }
+  }
+
+  // Return the best cutting pattern found
+  population.sort((a, b) => a.fitness - b.fitness);
+  return {
+    pattern: population[0].pattern,
+    waste: population[0].fitness,
+  };
+}
+
+module.exports = { fabricCuttingPlan };
